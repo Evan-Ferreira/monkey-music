e668bbe4f5fa2acb7372cd75b45a607634c07e71
Evan-Ferreira <evanjfer@gmail.com>
2024-08-18 08:23:36 -0400

    Final Push Before Making More Branches

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,37 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
 app.get('/', (req, res) => {
     res.send('Hello, TypeScript Node Express!');
+});
+
+app.get('/token', async (req, res) => {
+    try {
+        const response = await axios.post(
+            'https://accounts.spotify.com/api/token',
+            {
+                grant_type: 'client_credentials',
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            }
+        );
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 app.listen(port, () => {
