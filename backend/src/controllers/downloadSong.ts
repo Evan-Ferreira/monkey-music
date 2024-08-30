f6f67464df6c536ebaee4016d1d8224847881316
Evan-Ferreira <evanjfer@gmail.com>
2024-08-29 20:58:10 -0400

    Got YouTube audio download working

--- backend/src/controllers/downloadSong.ts
+++ backend/src/controllers/downloadSong.ts
@@ -1,24 +1,17 @@
-import ytdl from 'ytdl-core';
-import ffmpeg from 'fluent-ffmpeg';
-import readline from 'readline';
+import { Downloader } from 'ytdl-mp3';
 
 export const downloadMP3 = async () => {
     try {
-        const videoURL = `https://www.youtube.com/watch?v=${'_dK2tDK9grQ'}`;
-        const stream = ytdl('_dK2tDK9grQ', { quality: 'highestaudio' });
-        const start = Date.now();
-        ffmpeg(stream)
-            .audioBitrate(128)
-            .save(`${__dirname}/${'shape_of_you'}.mp3`)
-            .on('progress', (p) => {
-                readline.cursorTo(process.stdout, 0);
-                process.stdout.write(`${p.targetSize}kb downloaded`);
-            })
-            .on('end', () => {
-                console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
-            });
-        console.log('Downloaded in:', Date.now() - start);
+        const videoID = '_dK2tDK9grQ';
+        const downloader = new Downloader({
+            getTags: true,
+            outputDir: './src/controllers',
+        });
+
+        await downloader.downloadSong(
+            'https://www.youtube.com/watch?v=_dK2tDK9grQ'
+        );
     } catch (error) {
-        console.error('Failed to download song:', error);
+        console.error(error);
     }
 };
